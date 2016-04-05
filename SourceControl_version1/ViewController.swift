011b1289e948ad2dda91756717dbd934da8e22b9
Macintosh <macintosh@192.168.0.127>
2016-04-06 00:42:16 +0700

    add print

--- SourceControl_version1/ViewController.swift
+++ SourceControl_version1/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("heelo guy")
         // Do any additional setup after loading the view, typically from a nib.
     }
 

f9e70157b483e9fe2fa867dd7595df852402af25
Macintosh <macintosh@192.168.0.127>
2016-04-05 23:30:25 +0700

    Initial Commit

---
+++ SourceControl_version1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SourceControl_version1
+//
+//  Created by Macintosh on 4/5/2559 BE.
+//  Copyright © 2559 Macintosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
